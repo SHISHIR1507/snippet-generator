5d6f6c790e88bf963357cc356fb38cca176d4f1a
SHISHIR1507 <singhshishir4727@gmail.com>
2025-10-01 02:57:25 +0530

    displaying it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { prisma } from "@/lib/prisma";
 import Link from "next/link";
-export default function Home() {
+export default async function Home() {
+  const snippets = await prisma.snippet.findMany();
+
   return (
     <div>
       <h1 className="font-bold text-4xl"> Home </h1>
@@ -9,6 +11,15 @@ export default function Home() {
         <h1>Snippet</h1>
         <Link href={"/snippet/new"}><Button> New </Button></Link>
       </div>
+      {snippets.map((snippet) => (
+        <div key={snippet.id} className="flex items-center justify-between bg-gray-200 p-2 rounded-md my-2">
+          <h1>{snippet.title}</h1>
+          <Link href={`/snippet/${snippet.id}`}>
+            <Button variant={'link'}>View</Button>
+          </Link>
+
+        </div>
+      ))}
     </div>
   );
 }
